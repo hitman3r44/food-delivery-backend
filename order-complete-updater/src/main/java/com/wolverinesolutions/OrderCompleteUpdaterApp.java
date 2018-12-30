b262b15726aa856fef5f3f9815a09d3d42e3e5a9
hitman3r44 <hitman3r44@gmail.com>
2018-12-30 08:44:26 -0500

    Add new module called "order-complete-updater"

---
+++ order-complete-updater/src/main/java/com/wolverinesolutions/OrderCompleteUpdaterApp.java
@@ -0,0 +1,14 @@
+package com.wolverinesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@EnableCircuitBreaker
+public class OrderCompleteUpdaterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderCompleteUpdaterApp.class, args);
+    }
+}
